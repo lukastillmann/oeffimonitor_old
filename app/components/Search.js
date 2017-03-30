6fe82c6d73554310f79522668f0d07756861f727
Lukas Tillmann <home@lukastillmann.eu>
2017-03-30 13:39:28 +0200

    adds station search field to start page

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,27 +1,22 @@
 var React = require('react');
 var Select = require('react-select');
+var Fuse = require('fuse.js');
 import 'react-select/dist/react-select.css';
 
 const getStations = function() {
   return fetch('/wl.json')
     .then(function(response) {
         return response.json();
-    }).then(function(json) {
-        var arr = [];
-        arr = json.map(function(obj) {
-          return {
-            value: obj.stationID,
-            name: obj.name
-          } 
-        });
-        /*
-        arr = [
-          {value: 'one', name: 'One' },
-          {value: 'two', name:'Two'}
-        ];
-        */
-        console.log(arr);
-        return { options: arr};
+    })
+    .then(function(json) {
+      let arr = [];
+      arr = json.map(function(obj) {
+        return {
+          value: obj.stationID,
+          name: obj.name
+        } 
+      });
+      return arr;
     });
 }
 
@@ -30,40 +25,76 @@ const testOptions = [
       {value: 'two', name:'Two'}
     ];
 
-
-var Search = React.createClass({
-  render: function() {
-    return (
-      <div className="search">
-        <div className="search__title">Haltestelle suchen</div>
-        <input type="text" className="search__input"/>
-        <div className="search__results">
-        </div>
-      </div>
-    )
-  }
-});
-
 var SearchContainer = React.createClass({
-  onChange: function(event) {
-    console.log('change');
+  getInitialState: function() {
+    var results = {
+      name: '',
+      value: ''
+    };
+    return results;
   },
-  logChange: function(val) {
-    console.log("Selected: " + val.name);
+  handleSearchInput: function(event) {
+    event.persist()
+    var that = this;
+    getStations()
+      .then(function(stations) {
+        var options = {
+          shouldSort: true,
+          threshold: 0.2,
+          location: 0,
+          distance: 100,
+          maxPatternLength: 32,
+          minMatchCharLength: 1,
+          keys: [
+              "name"
+          ]
+        };
+        var fuse = new Fuse(stations, options); // "list" is the item array
+        var result = fuse.search(event.target.value);
+        that.setState({results: result});
+      });
+    //this.searchItems(event.target.value);
   },
   render: function() {
     return (
       <div>
-        <Search />
-        <Select.Async
-          name="form-field-name"
-          loadOptions = {getStations}
-          onChange={this.logChange}
-        />
+        <SearchInput onKeyUp={this.handleSearchInput}/>
+        <SearchResults results={this.state.results}/>
       </div>
       )
   }
 });
 
+var SearchInput = React.createClass({
+  render: function() {
+    return (
+      <div className="search">
+        <div className="search__title">Haltestelle suchen</div>
+        <input type="text" className="search__input" onKeyUp={this.props.onKeyUp}/>
+      </div>
+    )
+  }
+});
+
+var SearchResults = React.createClass({
+  listItems: function() {
+    if (this.props.results) {
+      return this.props.results.map(function(item) {
+        return <li key={item.value}>{item.name}</li>
+      });
+    }
+  },
+  render: function() {
+    var list = this.listItems();
+    return (
+      <div>
+      results
+      <ul>
+      {list}
+      </ul>
+      </div>
+    )
+  }
+})
 
 module.exports = SearchContainer;
